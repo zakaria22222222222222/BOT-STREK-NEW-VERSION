e101b974831d3869d90b639796530b511a1dfb22
zakaria22222222222222 <71077001+zakaria22222222222222@users.noreply.github.com>
2020-09-10 13:29:58 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzUwMDA5MzQ1OTA2ODM1NTU3.X00Sog.WmaHU4YbjXuKq1UV0kfJFxp_c6A", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["715558366037934121"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
